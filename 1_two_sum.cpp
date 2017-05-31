04d623f1a5e0bfe3ca15885c1173eb196e8454d4
U-5C5D2C8F2E6B308\ljw <txwd0033@163.com>
2017-05-31 10:51:30 +0800

    1 accept

--- 1_two_sum.cpp
+++ 1_two_sum.cpp
@@ -2,9 +2,22 @@
 #include <map>
 #include <algorithm>
 #include <iostream>
+#include <iterator>
 
 using std::multimap;
 using std::vector;
+
+/*
+问题1. 	std::copy(case1, case1 + 3, std::back_inserter(nums));， 如果是 std::copy(case1, case1 + 3, nums.begin()); 会出core；
+		因为目标容器的大小必须大于等于源容器的大小；
+问题2. 	multimap的使用， equal_range用来找到对应的key所有的kv对；insert插入 pair对。
+问题3.  刚开始用的是map， 对于一个值在不同位置出现多次的问题不能解决；
+问题4. 	数组定义  int a[] = { 1, 2, 3, 4};
+
+
+
+
+*/
 
 class Solution {
 public:
@@ -21,25 +34,37 @@ public:
 				if (iter_start->second != v->second) {
 					v_ret.push_back(v->second);
 					v_ret.push_back(iter_start->second);	
-					break;
+					std::sort(v_ret.begin(), v_ret.end());
+					return v_ret;
 				}
 			}
 		}
-		std::sort(v_ret.begin(), v_ret.end());
+		
 		return v_ret;
     }
 };
 
 int main() {
 	std::vector<int> nums;
-	std::cout << "haha2";
+	
 	int case1[] = {2, 7, 11, 15};
-	std::cout << "haha1";
-	std::copy(case1, case1+3, nums.begin());
+	
+	std::copy(case1, case1 + 3, std::back_inserter(nums));
 	Solution s;
-	std::cout << "haha";
+	
 	std::vector<int> ret = s.twoSum(nums, 9);
-	// for (int i = 0; i < ret.size(); ++i) {
-	// 	std::cout << ret[i] << std::endl;
-	// }
+	for (int i = 0; i < ret.size(); ++i) {
+		std::cout << ret[i] << std::endl;
+	}
+
+	int case2[] = { 3, 3};
+	nums.clear();
+	std::cout <<"case2" <<std::endl;
+	std::copy(case2, case2 + 2, std::back_inserter(nums));
+	ret = s.twoSum(nums, 6);
+	for (int i = 0; i < ret.size(); ++i) {
+		std::cout << ret[i] << std::endl;
+	}	
+
+
 }
